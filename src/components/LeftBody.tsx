6b4be207fc264730b7eb88e8062d458f589f98b8
trungtran-ptn <122074057+trungtranptn@users.noreply.github.com>
2023-03-17 17:15:34 +0700

    use map for loop array

--- src/components/LeftBody.tsx
+++ src/components/LeftBody.tsx
@@ -3,14 +3,29 @@ import { Content } from "./Content";
 
 type props = {};
 
+const contents = [
+  {
+    title: "Content 1",
+    description: "Learn React",
+  },
+  {
+    title: "Content 2",
+    description: "Learn TypeScripts",
+  },
+  {
+    title: "Content 3",
+    description: "Learn Vite",
+  },
+];
+
 export const LeftBody = (props: props) => {
   return (
     <div className="width-70">
       <BigTitle />
       <div className="border width-80">
-        <Content title="Content 1" description="Learn React" />
-        <Content title="Content 2" description="Learn TypeScripts" />
-        <Content title="Content 3" description="Learn Vite" />
+        {contents.map((content) => (
+          <Content title={content.title} description={content.description} />
+        ))}
       </div>
     </div>
   );
