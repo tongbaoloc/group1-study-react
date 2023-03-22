f5ee75339126f5e904363e2c23e83b92433c87dc
Phuc.Tran <phuc.tran@ptnglobalcorp.com>
2023-03-23 06:03:55 +0700

    exercise 2: website todo

--- first-vite-react/src/App.tsx
+++ first-vite-react/src/App.tsx
@@ -1,18 +1,20 @@
-import './App.css'
-import BoxesContainer from './components/BoxesContainer'
-import Content from './components/Content'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import { Button } from '@mui/material';
+import './App.css';
+import Title from './components/ex2/title/Title';
+import Todo from './components/ex2/todo/Todo';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Content />
-      <BoxesContainer />
-      <Footer />
-    </>
-  )
+    <div className="flex flex-col items-center">
+      <Title />
+      <Todo />
+      <Button variant="contained">
+        <AddIcon />
+        New task
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ first-vite-react/src/components/ex2/todo/Todo.tsx
@@ -0,0 +1,5 @@
+function Todo() {
+  return <>todo</>;
+}
+
+export default Todo;
