fc468f40c20d1390d2dc08fda6c4a8a93a49883c
trungtran-ptn <122074057+trungtranptn@users.noreply.github.com>
2023-03-29 13:27:13 +0700

    using axios get all task from mockAPI

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import { v4 as uuidv4 } from "uuid";
 import { Button } from "./components/Button";
 import { Header } from "./components/Header";
 import { Todos } from "./components/Todos";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskType } from "./types/TaskType";
 import { Input } from "./components/Input";
-
+import axios from "axios";
 function App() {
-  const [data, setData] = useState<Array<TaskType>>([
-    {
-      id: uuidv4(),
-      name: "React",
-    },
-    {
-      id: uuidv4(),
-      name: "Vite",
-    },
-  ]);
+  const [data, setData] = useState<Array<TaskType>>([]);
 
+  const getAllTask = () => {
+    axios.get(`https://6423d862d6152a4d48008bd3.mockapi.io/todos`)
+      .then(res => {
+        setData(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getAllTask()
+  }, [])
+  
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const handleDeleteTask = (id: string) => {
